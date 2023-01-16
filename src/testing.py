cfffbc7135bd7663af5d2c497128166f57c627e9
Aw3ra <aw3ra@proton.me>
2023-01-17 07:18:06 +1100

    modified:   src/testing.py 	modified:   src/__pycache__/discord_stuff.cpython-310.pyc 	modified:   src/__pycache__/generate_tweet.cpython-310.pyc 	modified:   src/__pycache__/tweets_class.cpython-310.pyc 	modified:   src/data/tweets.csv 	modified:   src/discord_stuff.py 	modified:   src/generate_tweet.py 	modified:   src/main.py 	modified:   src/tweets_class.py
    Added a function to check sentiment in the generate_tweet file. This function is called in the main file and the sentiment is added to the tweet. The sentiment is also added to the csv file.
    Amended the main.py to call a function for retrieving tweets, hopefully get this running every hour.

--- src/testing.py
+++ src/testing.py
@@ -1,8 +1,36 @@
 import discord_stuff
 import generate_tweet
+import datetime
+import csv
 
-test_tweet = 'This is a test tweet with a bunch of info on how to get the tweets, im adding some extra characters so the tweet gets longer and longer to test the validity of making longer tweets'
+test_tweet = 'I hate how the team at solana does their marketing, if only someone were to come in and manage social media.'
+
+# Function to open the csv file
+# Inputs:  csv_name - the name of the csv file
+# Outputs: list_of_tweets_ - a list of tweets
+def open_csv(csv_name):
+    # Open the csv file
+    with open(csv_name, 'r', encoding = 'utf-8') as file:
+        # Create a first 'dummy' tweet
+        firstTweet = [00000,'This is a fake tweet',30, datetime.datetime(2020, 1, 1, 0, 0)]
+        # Create a list of tweets
+        list_of_tweets_ = []
+        # Add the first tweet to the list of tweets
+        list_of_tweets_.append(firstTweet)
+        # Create a csv reader
+        reader = csv.reader(file)
+        # Skip the header
+        next(reader)
+        # For each row in the csv file
+        for row in reader:
+            # Add the row to the list of tweets
+            list_of_tweets_.append(row)
+    # Return the list of tweets
+    return list_of_tweets_
+
+list_of_tweets = open_csv('src/data/tweets.csv')
 
 
-response = generate_tweet.get_response(test_tweet, 'Doug')
-print(response)
+for tweet in list_of_tweets:
+    response = generate_tweet.get_sentiment(tweet[1])
+    print(response)
